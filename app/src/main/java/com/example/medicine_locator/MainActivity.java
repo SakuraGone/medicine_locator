469e9c5f6b27302480a43bc49e0b7f43420e8341
SakuraGone <sakuragone@gmail.com>
2022-08-09 19:55:43 -0400

    Add connection between MainActivity and SearchActivity

--- app/src/main/java/com/example/medicine_locator/MainActivity.java
+++ app/src/main/java/com/example/medicine_locator/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.medicine_locator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button search_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_button = (Button) findViewById(R.id.search_button);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitySearch();
+            }
+        });
     }
+
+    public void openActivitySearch() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
 }
