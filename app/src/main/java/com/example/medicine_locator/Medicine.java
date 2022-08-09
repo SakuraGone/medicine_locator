abc8129beb53a4c3217c878c2253d52c65cab6e5
SakuraGone <sakuragone@gmail.com>
2022-08-09 18:35:41 -0400

    Create Medicine class

---
+++ app/src/main/java/com/example/medicine_locator/Medicine.java
@@ -0,0 +1,28 @@
+package com.example.medicine_locator;
+
+public class Medicine {
+    //medicine basic information
+    private String medicineName;
+    private String medicineLocation;
+
+    public Medicine(String name, String location) {
+        this.medicineName = name;
+        this.medicineLocation = location;
+    }
+
+    public String getMedicineName() {
+        return this.medicineName;
+    }
+
+    public String getMedicineLocation() {
+        return this.medicineLocation;
+    }
+
+    public void setMedicineName(String name) {
+        this.medicineName = name;
+    }
+
+    public void setMedicineLocation(String location) {
+        this.medicineLocation = location;
+    }
+}
