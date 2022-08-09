35d2f90eb6a93a3bd6994299549252416f8b23f8
SakuraGone <sakuragone@gmail.com>
2022-08-09 19:30:52 -0400

    Update SearchActivity

--- app/src/main/java/com/example/medicine_locator/SearchActivity.java
+++ app/src/main/java/com/example/medicine_locator/SearchActivity.java
@@ -3,12 +3,61 @@ package com.example.medicine_locator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Map;
+
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    Map<String, String> map;
+    String[] medNameList;
+    String[] medLocationList;
+    ArrayList<Medicine> arraylist = new ArrayList<Medicine>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        medNameList = new String[]{"感冒灵", "布洛芬"};
+        medLocationList = new String[]{"1楼2行3列", "2楼3行1列"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < medNameList.length; i++) {
+            Medicine med = new Medicine(medNameList[i], medLocationList[i]);
+            // Binds all strings into an array
+            arraylist.add(med);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
     }
 }
