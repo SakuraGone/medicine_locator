1d014588f4eb36b76f3c0c22edf1752cd916a271
SakuraGone <sakuragone@gmail.com>
2022-08-10 18:04:25 -0400

    Add on conflict action when data insert exists in database

--- app/src/main/java/com/example/medicine_locator/data/MedicineDao.java
+++ app/src/main/java/com/example/medicine_locator/data/MedicineDao.java
@@ -3,6 +3,7 @@ package com.example.medicine_locator.data;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Dao
 public interface MedicineDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(Medicine medicine);
 
     @Delete
